92359eff2b037a0ba8a154883ef2784e81a1eb2a
nico-rodriguez <marconicolasrodriguez@gmail.com>
2019-04-15 00:17:35 -0300

    Se quitan tests para casos extraños.

--- test.pl
+++ test.pl
@@ -51,9 +51,7 @@ test(enesimo_ejemplo_2) :- enesimo([5, 2, [3, 1], 7], 3, [3, 1]).
 test(sublista4_ejemplo) :- sublista([5, 2, 3, 1, 7], [2, 3, 1], 2, 4).
 test(sublista_caso_1) :- sublista([1, 2, 3, 4], [2, 3], I, J), I = 2, J = 3.
 test(sublista_caso_2) :- sublista([1, 2, 3, 4], Sub, 2, 3), Sub = [2, 3].
-test(sublista_caso_3) :- sublista(L, [2, 3], 2, J), L = [_, 2, 3|_], J = 3.
-test(sublista_caso_4) :- sublista(L, [2, 3], I, 3), L = [_, 2, 3|_], I = 2.
-
-
+%% test(sublista_caso_3) :- sublista(L, [2, 3], 2, J), L = [_, 2, 3|_], J = 3.
+%% test(sublista_caso_4) :- sublista(L, [2, 3], I, 3), L = [_, 2, 3|_], I = 2.
 
 :- end_tests(lab1).
